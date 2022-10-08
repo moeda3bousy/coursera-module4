6cfd453bcca3092787dcfd44d06b0fa9d403355f
Mohamed El-Daabousy <93544032+moeda3bousy@users.noreply.github.com>
2022-10-08 18:15:20 +0200

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,9 @@
+(function(window){
+var helloSpeaker = {};
+window.helloSpeaker = helloSpeaker;
+var speakWord = "Hello";
+helloSpeaker.speak= function(name) {
+  console.log(speakWord + " " + name);
+}
+})(window);
+
